c2ca528f93599e0cb0f8086ad04fc2e9d719808a
improvisor-arafat <physics.propagation>
2023-02-19 20:53:52 +0530

    __

---
+++ welcome.js
@@ -0,0 +1,5 @@
+function playButton(){
+    document.querySelector('.start-game').style.display= "block";
+    document.querySelector('.loader').style.display= "none";
+}
+setTimeout(playButton, 2000);
